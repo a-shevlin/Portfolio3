841931e7f8fd7d5fab499da49b2ce937965673d7
a-shev <alexshevlin1@gmail.com>
2023-02-24 20:55:39 -0800

    add image and wrapper to right div.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classes from './Home.module.scss';
 import Layout from '../components/Layout';
+import alex from '../assets/alex-headshot.png';
+import { motion } from 'framer-motion';
 
 function Home() {
   return (
@@ -16,7 +18,12 @@ function Home() {
               <h4 className={classes.j_title}>Full Stack Developer</h4>
             </div>
             <div className={classes.right}>
-
+              <motion.div 
+                className={classes.img_wrapper}
+                
+              >
+                <img className={classes.alex} src={alex} alt='alex shevlin' />
+              </motion.div>
             </div>
           </div>
         </div>
