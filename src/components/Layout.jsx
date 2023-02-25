8789e6344129ec6cd81e036d1be319b019a1cc86
a-shev <alexshevlin1@gmail.com>
2023-02-24 20:52:58 -0800

    move Nav placement to inside layout.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import classes from './Layout.module.scss';
+import Nav from './Nav';
 
 function Layout({ children }) {
   return (
-    <motion.div 
-      className={classes.container}
-      initial={{opacity: 0}}
-      animate={{opacity: 1}}
-      exit={{opacity: 0}}>
-        {children}
-    </motion.div>
+    <React.Fragment>
+      <Nav />
+      <motion.div 
+        className={classes.container}
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        exit={{opacity: 0}}>
+          {children}
+      </motion.div>
+    </React.Fragment>
   )
 }
 
