7b72d3b91380d1020dcdca302543c0d55de524fe
a-shev <alexshevlin1@gmail.com>
2023-02-26 16:27:08 -0800

    create home page fade in.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './Home.module.scss';
+import styles from './Home.module.scss';
 import Layout from '../components/Layout';
 import alex from '../assets/alex-headshot.png';
 import { motion } from 'framer-motion';
@@ -8,21 +8,54 @@ function Home() {
   return (
     <React.Fragment>
       <Layout>
-        <div className={classes.group}>
+        <div className={styles.group}>
 
-          <div className={classes.grid}>
-            <div className={classes.left}>
-              <h3> 
-                Hi, I'm <span className={classes.name}>Alex Shevlin</span>
-              </h3>
-              <h4 className={classes.j_title}>Full Stack Developer</h4>
+          <div className={styles.grid}>
+            <div className={styles.left}>
+              <motion.h3
+                initial={{
+                  opacity: 0
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  delay: 0.2,
+                  duration: 1,
+                }}
+              > 
+                Hi, I'm <span className={styles.name}>Alex Shevlin</span>
+              </motion.h3>
+              <motion.h4 
+                className={styles.j_title}
+                initial={{
+                  opacity: 0
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  delay: 0.5,
+                  duration: 1,
+                }}
+                >
+                  Full Stack Developer</motion.h4>
             </div>
-            <div className={classes.right}>
+            <div className={styles.right}>
               <motion.div 
-                className={classes.img_wrapper}
-                
+                className={styles.img_wrapper}
+                initial={{
+                  opacity: 0
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  delay: 1,
+                  duration: 1,
+                }}
               >
-                <img className={classes.alex} src={alex} alt='alex shevlin' />
+                <img className={styles.alex} src={alex} alt='alex shevlin' />
               </motion.div>
             </div>
           </div>
