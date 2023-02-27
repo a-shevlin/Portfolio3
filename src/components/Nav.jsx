588d820bcba12dbd822becaa848649eb37a014de
a-shev <alexshevlin1@gmail.com>
2023-02-26 16:25:39 -0800

    add mobile support for Nav elements.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import classes from './Nav.module.scss';
+import styles from './Nav.module.scss';
 import { motion } from 'framer-motion';
 
 
 function Nav() {
   const [width, setWidth] = useState(false);
   const [navOpen, setNavOpen] = useState(false);
-
   useEffect(() => {
     window.onresize = () => {
       (window.innerWidth < 800) ? setWidth(true) : setWidth(false);
     }
   }, []);
 
+  
   return (
     <React.Fragment>
       <nav>
-          <div>
+        <div className={styles.browser_flex}>
             <motion.h1
               initial={{
                 opacity: 0
@@ -33,7 +33,7 @@ function Nav() {
               <Link to={'/'}>as</Link>
             </motion.h1> 
           { !width ? 
-            <ul>
+            <ul className={styles.browser_list_items}>
               <motion.li
                 initial={{
                   opacity: 0
@@ -78,9 +78,9 @@ function Nav() {
               </motion.li>
             </ul>
           :
-            <div className={classes.phone}>
-              <button className={classes.nav_button}>
-              <motion.svg className={classes.icon} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="matrix(-1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier">
+            <div className={styles.phone}>
+              <button className={styles.nav_button} onClick={() => setNavOpen(!navOpen)}>
+                <motion.svg className={styles.icon} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="matrix(-1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier">
                   <motion.path 
                     initial={{
                       pathLength: 0,
@@ -109,7 +109,7 @@ function Nav() {
                       duration: 0.8,
                     }}
                     d="M4 12L16 12" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></motion.path>
-                    <motion.path 
+                  <motion.path 
                     initial={{
                       pathLength: 0,
                       opacity: 0,
@@ -122,10 +122,54 @@ function Nav() {
                       delay: 0.2,
                       duration: 0.5,
                     }}
-                    d="M4 6L20 6" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></motion.path> </g></motion.svg>
+                    d="M4 6L20 6" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></motion.path> </g>
+                </motion.svg>
               </button>
-              <div className={(navOpen ? classes.nav_open : classes.nav_closed)}>
-
+              <div className={(navOpen ? styles.nav_open : styles.nav_closed)}>
+              <ul className={styles.mobile_list_item}>
+              <motion.li
+                initial={{
+                  opacity: 0
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  delay: 0.2,
+                  duration: 1,
+                }}
+              >
+                <Link to={'/about'}>About</Link>
+              </motion.li>
+              <motion.li
+                initial={{
+                  opacity: 0
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  delay: 0.2,
+                  duration: 1,
+                }}
+              >
+                <Link to={'/projects'}>Projects</Link>
+              </motion.li>
+              <motion.li
+                initial={{
+                  opacity: 0
+                }}
+                animate={{
+                  opacity: 1,
+                }}
+                transition={{
+                  delay: 0.2,
+                  duration: 1,
+                }}
+              >
+                <Link to={'/contact'}>Contact</Link>
+              </motion.li>
+            </ul>
               </div>
             </div>
         } 
