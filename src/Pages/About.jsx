b9364b593f91f1b4c4e641bf599ebd96f8adae76
a-shev <alexshevlin1@gmail.com>
2023-02-26 16:26:51 -0800

    create Project and About components.

---
+++ src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+function About() {
+  return (
+    <Layout>
+      <h1 style={{ fontSize: '12rem'}}>ABOUT PAGE</h1>
+    </Layout>
+  )
+}
+
+export default About
