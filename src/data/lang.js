1f27329072ecfce6c042cb9f5b21f9fa62e6af38
a-shev <alexshevlin1@gmail.com>
2023-02-26 16:26:35 -0800

    WIP technologies for carousel.

---
+++ src/data/lang.js
@@ -0,0 +1,59 @@
+const data = [
+  {
+    id: 0,
+    name: 'JavaScript',
+    img: '',
+  },
+  {
+    id: 1,
+    name: 'React.js',
+    img: '',
+  },
+  {
+    id: 2,
+    name: 'CSS',
+    img: '',
+  },
+  {
+    id: 3,
+    name: 'Sass',
+    img: '',
+  },
+  {
+    id: 4,
+    name: 'HTML',
+    img: '',
+  },
+  {
+    id: 5,
+    name: 'C#',
+    img: '',
+  },
+  {
+    id: 6,
+    name: '.ASP/.NET',
+    img: '',
+  },
+  {
+    id: 7,
+    name: 'Java',
+    img: '',
+  },
+  {
+    id: 8,
+    name: 'TypeScript',
+    img: '',
+  },
+  {
+    id: 9,
+    name: 'Python',
+    img: '',
+  },
+  {
+    id: 10,
+    name: '',
+    img: '',
+  },
+]
+
+export default data;
---
+++ src/data/service.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    id: 0,
+    name: 'AWS Lambda',
+    img: '',
+  },
+  {
+    id: 1,
+    name: 'MySQL',
+    img: '',
+  },
+  {
+    id: 2,
+    name: 'MongoDB',
+    img: '',
+  },
+  {
+    id: 3,
+    name: 'Firestore',
+    img: '',
+  },
+  {
+    id: 4,
+    name: 'Node.js',
+    img: '',
+  },
+  {
+    id: 5,
+    name: 'Express',
+    img: '',
+  },
+  {
+    id: 6,
+    name: 'Apache Maven',
+    img: '',
+  },
+  {
+    id: 7,
+    name: 'VSCode',
+    img: '',
+  },
+  {
+    id: 8,
+    name: 'IntelliJ',
+    img: '',
+  },
+  {
+    id: 9,
+    name: 'Docker',
+    img: '',
+  },
+  {
+    id: 10,
+    name: '',
+    img: '',
+  },
+  {
+    id: 11,
+    name: '',
+    img: '',
+  },
+  {
+    id: 12,
+    name: '',
+    img: '',
+  },
+  {
+    id: 13,
+    name: '',
+    img: '',
+  },
+]
+
+export default data;
