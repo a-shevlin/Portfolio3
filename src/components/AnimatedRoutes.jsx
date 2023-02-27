e5ff7865210711202221d4f9bff9c2458d0bf01b
a-shev <alexshevlin1@gmail.com>
2023-02-26 16:24:22 -0800

    add Project and About, remove Nav.

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { AnimatePresence } from 'framer-motion';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from '../Pages/Home';
+import About from '../Pages/About';
+import Nav from './Nav';
+import Projects from '../Pages/Projects';
 
 
 function AnimatedRoutes() {
   const location =  useLocation();
   return (
     <AnimatePresence>
+      <Nav />
       <Routes location={location} key={location.pathname}>
         <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Projects />} />
       </Routes>
     </AnimatePresence>
   )
