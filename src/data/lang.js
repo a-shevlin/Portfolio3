77f5edcb35a44795e1288578f31462c87ab6cfa4
a-shev <alexshevlin1@gmail.com>
2023-02-28 01:28:34 -0800

    add assets to data files.

--- src/data/lang.js
+++ src/data/lang.js
@@ -1,53 +1,65 @@
+import javascript from '../assets/techs/javascript.png';
+import react from '../assets/techs/React-icon.svg.png';
+import css from '../assets/techs/css3.png';
+import sass from '../assets/techs/sass.png';
+import html from '../assets/techs/html5.png';
+import csharp from '../assets/techs/csharp.png';
+import asp from '../assets/techs/aspnet.png';
+import java from '../assets/techs/java.png';
+import typescript from '../assets/techs/ts.png';
+import python from '../assets/techs/python.png';
+
 const data = [
   {
     id: 0,
-    name: 'JavaScript',
-    img: 'https://1000logos.net/wp-content/uploads/2020/09/JavaScript-Logo-500x313.png',
+    name: 'HTML',
+    img: html,
   },
   {
     id: 1,
-    name: 'React.js',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+    name: 'JavaScript',
+    img: javascript,
   },
   {
     id: 2,
     name: 'CSS',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1200px-CSS3_logo_and_wordmark.svg.png',
+    img: css,
   },
   {
     id: 3,
-    name: 'Sass',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/2560px-Sass_Logo_Color.svg.png',
+    name: 'React.js',
+    img: react,
   },
   {
     id: 4,
-    name: 'HTML',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/640px-HTML5_logo_and_wordmark.svg.png',
+    name: 'Sass',
+    img: sass,
   },
+  
   {
     id: 5,
     name: 'C#',
-    img: 'https://images.saymedia-content.com/.image/t_share/MTc0NDczMTM4OTg0NDYxOTU4/a-brief-introduction-to-c.png',
+    img: csharp,
   },
   {
     id: 6,
     name: '.ASP/.NET',
-    img: 'https://global-uploads.webflow.com/6047a9e35e5dc54ac86ddd90/63064f1fedf422395124660e_e7d03466.png',
+    img: asp,
   },
   {
     id: 7,
     name: 'Java',
-    img: 'https://logos-world.net/wp-content/uploads/2022/07/Java-Logo.png',
+    img: java,
   },
   {
     id: 8,
     name: 'TypeScript',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
+    img: typescript,
   },
   {
     id: 9,
     name: 'Python',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png',
+    img: python,
   },
   // {
   //   id: 10,
--- src/data/service.js
+++ src/data/service.js
@@ -1,49 +1,58 @@
+import lambda from '../assets/techs/aws-lambda.png';
+import mysql from '../assets/techs/Mysql_logo.png';
+import mongodb from '../assets/techs/MongoDB_Logo.svg.png';
+import firestore from '../assets/techs/firestore.png';
+import node from '../assets/techs/nodejs.png';
+import maven from '../assets/techs/maven.png';
+import vscode from '../assets/techs/vscode.png';
+import intellij from '../assets/techs/IntelliJ.png';
+
 const data = [
   {
     id: 0,
     name: 'AWS Lambda',
-    img: 'https://cdn.cdnlogo.com/logos/a/47/aws-lambda.svg',
+    img: lambda,
   },
   {
     id: 1,
     name: 'MySQL',
-    img: 'https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png',
+    img: mysql,
   },
   {
     id: 2,
     name: 'MongoDB',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/MongoDB_Logo.svg/2560px-MongoDB_Logo.svg.png',
+    img: mongodb,
   },
   {
     id: 3,
     name: 'Firestore',
-    img: 'https://miro.medium.com/max/1400/1*_akvjplI8xbMr3_VxcWeLQ.png',
+    img: firestore,
   },
   {
     id: 4,
     name: 'Node.js',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png',
+    img: node,
   },
   {
     id: 5,
     name: 'Apache Maven',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Apache_Maven_logo.svg/2560px-Apache_Maven_logo.svg.png',
+    img: maven,
   },
   {
     id: 6,
     name: 'VSCode',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png',
+    img: vscode,
   },
   {
     id: 7,
     name: 'IntelliJ',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/IntelliJ_IDEA_Icon.svg/1024px-IntelliJ_IDEA_Icon.svg.png',
+    img: intellij,
   },
-  {
-    id: 8,
-    name: 'Docker',
-    img: 'https://www.docker.com/wp-content/uploads/2022/03/vertical-logo-monochromatic.png',
-  },
+  // {
+  //   id: 8,
+  //   name: 'Docker',
+  //   img: 'https://www.docker.com/wp-content/uploads/2022/03/vertical-logo-monochromatic.png',
+  // },
   // {
   //   id: 10,
   //   name: '',
