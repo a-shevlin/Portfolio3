4fb679d87039190b527e2d2d5bed3b49d3a6c931
a-shev <alexshevlin1@gmail.com>
2023-02-28 02:46:43 -0800

    add contact and remove about.

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -3,6 +3,7 @@ import { AnimatePresence } from 'framer-motion';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from '../Pages/Home';
 import About from '../Pages/About';
+import Contact from '../Pages/Contact';
 import Nav from './Nav';
 import Projects from '../Pages/Projects';
 
@@ -14,8 +15,9 @@ function AnimatedRoutes() {
       <Nav />
       <Routes location={location} key={location.pathname}>
         <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />} /> */}
         <Route path="/project" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </AnimatePresence>
   )
